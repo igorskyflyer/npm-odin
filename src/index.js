8193091ccc00ee3b02269277b6a02e1453998eac
igorskyflyer <igor.dvlpr@gmail.com>
2022-09-11 00:11:00 +0200

    Update JSDoc

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@
  * NOTE: this function uses the method of shallow copying of the source object, for deep/true copied objects consider using [clone.js](https://github.com/igorskyflyer/npm-clone-js).
  * @param {object} target The target object to copy properties to.
  * @param {object} source The source object to copy properties from.
- * @returns {object} Returns the target object. If the **_source_** object is not provided the **_target_** object is returned unchanged.
+ * @returns {object} Returns the target object.
+ *
+ * If the **_target_** object is not provided and empty object is returned.
+ *
+ * If the **_source_** object is not provided the **_target_** object is returned unchanged.
  */
 function odin(target, source) {
   if (typeof target !== 'object') {
