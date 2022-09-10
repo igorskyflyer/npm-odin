582a73e4a3153701248fd8a9d8516ded87f46b63
igorskyflyer <igor.dvlpr@gmail.com>
2022-09-11 00:05:11 +0200

    Add tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,22 @@
 const chai = require('chai').assert
+const { odin } = require('../src/index.js')
 
-describe('🧪 $1 tests 🧪', () => {
-  it('#1 should return ', () => {
-    // chai.<>
+describe('🧪 Odin tests 🧪', () => {
+  it('#1 should return true', () => {
+    chai.isEmpty(odin({}, {}))
   })
+
+  it('#2 should return true', () => {
+    // @ts-ignore
+    chai.isObject(odin({}))
+  })
+
+  it('#3 should return true', () => {
+    // @ts-ignore
+    chai.isNull(odin())
+  })
+
+  it('#4 should return true', () => {
+    chai.deepEqual(odin({ a: 1, b: 2 }, { c: 3 }), { a: 1, b: 2, c: 3 })
+  })
+})
